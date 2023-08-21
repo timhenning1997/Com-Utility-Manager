12bfab28148807bcbe4547ad1402b64c0536d6a7
sturm-TUD <sebastian.sturm1@tu-dresden.de>
2023-08-21 13:25:16 +0200

    Added POL12 and PLO8 as calibration function

--- CalibrationFunctions.py
+++ CalibrationFunctions.py
@@ -9,6 +9,25 @@ def applyCalibrationFunction(calData, data, calibratedData):
             res += coeff[i] * val ** i
         calibratedData["UUID"].append(calData[0])
         calibratedData["DATA"].append(res)
+    
+    elif calData[3] == "POL12":
+        coeff = calData[2]              # Liste der vorhandenen Koeffizienten
+        val = int(data, 16) / 4095      # Rohdaten Wert
+        res = 0                         # Resultat auf 0 setzen
+        for i in range(0, len(coeff)):
+            res += coeff[i] * val ** i
+        calibratedData["UUID"].append(calData[0])
+        calibratedData["DATA"].append(res)
+    
+    elif calData[3] == "POL8":
+        coeff = calData[2]              # Liste der vorhandenen Koeffizienten
+        val = int(data, 16) / 255       # Rohdaten Wert
+        res = 0                         # Resultat auf 0 setzen
+        for i in range(0, len(coeff)):
+            res += coeff[i] * val ** i
+        calibratedData["UUID"].append(calData[0])
+        calibratedData["DATA"].append(res)
+    
     else:
         calibratedData["UUID"].append(calData[0])
         calibratedData["DATA"].append(int(data, 16))
